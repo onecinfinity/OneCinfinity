27bdeaabaec9e13b63d314af62604f99d83935e9
onecinfinity <onecinfinity@gmail.com>
2026-05-12 02:22:41 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -189,7 +189,6 @@ Promise.all([
     fetch("header.html").then(res => res.text()),
     fetch("footer.html").then(res => res.text()),
     fetch("sidebar.html").then(res => res.text()),
-    fetch("search-form.html").then(res => res.text())
   ])
   .then(([headerHTML, footerHTML, sidebarHTML, searchHTML]) => {
     $("#header").html(headerHTML);
